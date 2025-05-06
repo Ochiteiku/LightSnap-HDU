74dcc3458522cd657730ea6cfc5016ab7db08def
Ochiteiku <ochiteiku@outlook.com>
2025-05-06 10:26:37 +0800

    feat: add another video player test(by exoplayer)

---
+++ app/src/main/java/com/electroboys/lightsnap/ui/main/activity/VideoPlayActivity2.kt
@@ -0,0 +1,40 @@
+package com.electroboys.lightsnap.ui.main.activity
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
+import com.electroboys.lightsnap.R
+import androidx.core.net.toUri
+
+class VideoPlayActivity2 : AppCompatActivity() {
+    private lateinit var player: ExoPlayer
+    private lateinit var playerView: PlayerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_videoplay2)
+
+        // 初始化 PlayerView
+        playerView = findViewById(R.id.player_view)
+
+        // 初始化 ExoPlayer
+        player = ExoPlayer.Builder(this).build()
+        playerView.player = player
+
+        // 构建 Uri 指向 raw 文件
+        val videoUri = "android.resource://${packageName}/${R.raw.test_video}".toUri()
+        val mediaItem = MediaItem.fromUri(videoUri)
+
+        player.setMediaItem(mediaItem)
+        player.prepare()
+        player.play()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        player.release()
+    }
+}
