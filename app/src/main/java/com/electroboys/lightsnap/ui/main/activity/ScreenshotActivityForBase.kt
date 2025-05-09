dc5f2ff2a041d5a11b61a4e31f325556f1449190
Ochiteiku <ochiteiku@outlook.com>
2025-05-09 20:59:10 +0800

    feat:Select full screen with the space key on the screenshot activity

--- app/src/main/java/com/electroboys/lightsnap/ui/main/activity/ScreenshotActivityForBase.kt
+++ app/src/main/java/com/electroboys/lightsnap/ui/main/activity/ScreenshotActivityForBase.kt
@@ -3,6 +3,7 @@ package com.electroboys.lightsnap.ui.main.activity
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.PointF
+import android.view.KeyEvent
 import android.view.MotionEvent
 import android.view.View
 import android.widget.FrameLayout
@@ -41,6 +42,7 @@ class ScreenshotActivityForBase(private val activity: AppCompatActivity) {
         }
 
         Toast.makeText(activity, "按住屏幕进行拖动框选区域", Toast.LENGTH_SHORT).show()
+
 
         val touchListener = View.OnTouchListener { v, event ->
             when (event.action) {
@@ -88,8 +90,28 @@ class ScreenshotActivityForBase(private val activity: AppCompatActivity) {
             true
         }
 
+
+        //先将该页面设为焦点，方便快捷键监控，然后监控空格键，空格键则直接全屏截图
+        selectionOverlayView.isFocusableInTouchMode = true
+        selectionOverlayView.requestFocus()
+
+        selectionOverlayView.setOnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_SPACE && event.action == KeyEvent.ACTION_DOWN) {
+                val bitmap = ScreenshotUtil.captureWithStatusBar(activity)
+                onCaptureListener?.invoke(bitmap)
+
+                val container = activity.findViewById<FrameLayout>(android.R.id.content)
+                cleanup(container)
+                true
+            } else {
+                false
+            }
+        }
+
         selectionOverlayView.setOnTouchListener(touchListener)
+
     }
+
 
     private fun showMask() {
         maskView?.visibility = View.VISIBLE
